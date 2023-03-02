b6d0b0d8d189583cf40dcd643970ab598e9b09fe
Aztarok <aztarok2994@gmail.com>
2023-03-02 18:05:50 -0500

    On the final stage for the project!

--- src/index.js
+++ src/index.js
@@ -24,6 +24,43 @@ goalMaker("TOP", 0, "everyday", 1, 2);
 
 let slider = document.querySelector(".btn");
 slider.addEventListener("click", () => {
-    let content = document.querySelector(".slide");
-    content.classList.toggle("slide-out")
+    document.querySelector(".slide").classList.toggle("slide-out");
+    document.querySelector(".projects").classList.toggle("projects-out")
 })
+
+let modal = document.getElementById("myModal");
+
+let span = document.getElementsByClassName("close")[0];
+
+let newProject = document.querySelector(".new-project");
+newProject.addEventListener("click", () => {
+    modal.style.display = "flex";
+})
+
+span.addEventListener("click", () => {
+    modal.style.display = "none";
+})
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+let body = document.querySelector(".projects");
+let formSub = document.querySelector(".form-btn")
+console.log(formSub);
+
+formSub.addEventListener("click", (e) => {
+    e.preventDefault();
+    let projectForm = document.forms.ProjectForm;
+    let formData = new FormData(projectForm);
+
+    let projectName = formData.get("pname");
+    let projectPriority = formData.get("prior");
+    let div = document.createElement("div");
+    let para = document.createElement("h4");
+    para.textContent += projectName + projectPriority;
+    div.appendChild(para);
+    newProject.prepend(para);
+})
+
